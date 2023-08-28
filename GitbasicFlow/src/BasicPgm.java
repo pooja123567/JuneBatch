db261d680e35af3233c4a38d93f91e9589b5d802
Pooja <Pooja@DESKTOP-Q798BAO>
2023-08-29 00:12:56 +0530

    new one

--- GitbasicFlow/src/BasicPgm.java
+++ GitbasicFlow/src/BasicPgm.java
@@ -3,7 +3,7 @@ public class BasicPgm {
 
 	public static void main(String[] args) {
 	System.out.println("value");
-System.out.println("verification data");
+System.out.println("verification data of the party validation");
 	}
 
 }
